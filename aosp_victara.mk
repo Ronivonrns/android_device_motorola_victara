ef690b88481d496da1099554841dd43a8fb95490
Andersonmendes2016 <lge500meu@gmail.com>
2017-10-05 11:56:36 -0300

    victara: AEX Bringup

---
+++ aosp_victara.mk
@@ -0,0 +1,16 @@
+
+$(call inherit-product, device/motorola/victara/full_victara.mk)
+
+# Inherit some common AEX stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+PRODUCT_RELEASE_NAME := MOTO X (2014)
+PRODUCT_NAME := aosp_victara
+PRODUCT_MODEL := XT1095
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+PRODUCT_MODEL := Moto X 2014
+
+EXTENDED_BUILD_TYPE=OFFICIAL
